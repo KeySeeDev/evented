420993669f627ba268e7a02d9b95d831a520af47
StarStep <8550701+StarStep@users.noreply.github.com>
2019-01-13 23:17:56 +0100

    Update evented.js

--- evented.js
+++ evented.js
@@ -1,4 +1,4 @@
-var Eventer = {
+var Evented = {
     v: 0.1,
     events: {},
     filters: {},
@@ -12,81 +12,81 @@ var Eventer = {
      * @param {Int} overwrite_priority
      */
     on: function (name, funct, priority, overwrite_priority) {
-        if (typeof Eventer.events[name] != 'object')
-            Eventer.events[name] = {};
+        if (typeof Evented.events[name] != 'object')
+            Evented.events[name] = {};
         if (!overwrite_priority) {
             priority = !!priority ? priority : 50;
-            while (typeof Eventer.events[name][priority] == 'function') {
+            while (typeof Evented.events[name][priority] == 'function') {
                 priority++;
             }
         }
-        Eventer.events[name][priority] = funct;
+        Evented.events[name][priority] = funct;
         if (this.debug) {
-            console.log('Eventer.on', arguments);
+            console.log('Evented.on', arguments);
         }
-        return Eventer;
+        return Evented;
     },
     off: function (name, priority) {
-        (priority ? delete Eventer.events[name][priority] : delete Eventer.events[name])
+        (priority ? delete Evented.events[name][priority] : delete Evented.events[name])
     },
     /*
      * Do actions, the output will not be changed
-     Eventer.on('test', function(e,a,b,c,d,e,f,g) { console.log(e,a,b,c,d,e,f,g);});
-     Eventer.trigger('test', [1,2,3,4,5,6]);
+     Evented.on('test', function(e,a,b,c,d,e,f,g) { console.log(e,a,b,c,d,e,f,g);});
+     Evented.trigger('test', [1,2,3,4,5,6]);
      */
     trigger: function (name, args, context, async, is_filter) {
-        Eventer._run_functions(false, name, args, context, async, is_filter);
+        Evented._run_functions(false, name, args, context, async, is_filter);
         if (this.debug) {
-            console.log('Eventer.trigger', arguments);
+            console.log('Evented.trigger', arguments);
         }
     },
     /*
      * Do action and change the output
-     Eventer.add_filter('test', function(data) { data.addme += 2; return data; });
-     Eventer.filter('test', [{addme: 5, deleteme: true}]);
+     Evented.add_filter('test', function(data) { data.addme += 2; return data; });
+     Evented.filter('test', [{addme: 5, deleteme: true}]);
      */
     filter: function (name, args, context) {
-        return (Eventer.has_filters(name) ? Eventer._run_functions(true, name, args, context, false, true) : args[0]);
+        return (Evented.has_filters(name) ? Evented._run_functions(true, name, args, context, false, true) : args[0]);
         if (this.debug) {
-            console.log('Eventer.filter', arguments);
+            console.log('Evented.filter', arguments);
         }
     },
     has_filters: function (name) {
-        for (var key in Eventer.filters[name]) {
+        for (var key in Evented.filters[name]) {
             return true;
         }
         return false;
     },
     add_filter: function (name, funct, priority, overwrite_priority) {
-        if (typeof Eventer.filters[name] != 'object')
-            Eventer.filters[name] = {};
+        if (typeof Evented.filters[name] != 'object')
+            Evented.filters[name] = {};
         if (!overwrite_priority) {
             priority = !!priority ? priority : 50;
-            while (typeof Eventer.filters[name][priority] == 'function') {
+            while (typeof Evented.filters[name][priority] == 'function') {
                 priority++;
             }
         }
-        Eventer.filters[name][priority] = funct;
+        Evented.filters[name][priority] = funct;
         if (this.debug) {
-            console.log('Eventer.add_filter', arguments);
+            console.log('Evented.add_filter', arguments);
         }
     },
     remove_filter: function (name, priority) {
-        (priority ? delete Eventer.filters[name][priority] : delete Eventer.filters[name]);
+        (priority ? delete Evented.filters[name][priority] : delete Evented.filters[name]);
         if (this.debug) {
-            console.log('Eventer.remove_filter', arguments);
+            console.log('Evented.remove_filter', arguments);
         }
         return true;
     },
     _run_functions: function (is_filter, name, args, context, async) {
-        Eventer.stop = false;
-        var Type = is_filter ? Eventer.filters : Eventer.events;
+        Evented.stop = false;
+        var Type = is_filter ? Evented.filters : Evented.events;
         if (Type[name]) {
             if (typeof args == 'undefined') {
                 args = [];
             }
             if (is_filter == false) {
-                args.unshift({type: 'eventer'});
+                args.unshift({type: 'Evented'});
             }
             var keys = Object.keys(Type[name]);
             for (var priority = 0; priority < keys.length; priority++) {
@@ -98,7 +98,7 @@ var Eventer = {
                                 Type[name][the_key].apply(context, args);
                             }, 0);
                         } else {
-                            if (Eventer.stop == false) {
+                            if (Evented.stop == false) {
                                 if (is_filter) {
                                     filtered = Type[name][the_key].apply(context, args);
                                     if (typeof filtered !== 'undefined') {
